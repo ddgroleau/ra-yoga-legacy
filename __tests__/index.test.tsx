4fee396a6fd4cd8eb8d1e08ae4fafeb8df57caab
Dan Groleau <ddgroleau@gmail.com>
2022-12-20 12:18:25 -0500

    Add navbar, footer, and compliance pages

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import Home from '@/pages/index';
 
+jest.mock('next/router', () => ({
+    useRouter() {
+        return ({
+            route: '/',
+            pathname: '',
+            query: '',
+            asPath: '',
+            push: jest.fn(),
+            events: {
+                on: jest.fn(),
+                off: jest.fn()
+            },
+            beforePopState: jest.fn(() => null),
+            prefetch: jest.fn(() => null)
+        });
+    },
+}));
+
 describe('Home', () => {
     it('default test', () => {
         render(<Home />);
